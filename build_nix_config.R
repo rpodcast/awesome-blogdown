19f75159c8c9c42cdef1382d9e7ee1ad09e36278
Eric Nantz <theRcast@gmail.com>
2025-10-15 21:27:07 -0400

    chore: initialize nix dev environment

---
+++ build_nix_config.R
@@ -0,0 +1,18 @@
+#https://gist.github.com/b-rodrigues/d427703e76a112847616c864551d96a1
+library(rix)
+
+rix(
+  r_ver = "4.5.1",
+  project_path = getwd(),
+  r_pkgs = c(
+    "rmarkdown",
+    "blogdown",
+    "jsonlite",
+    "knitr",
+    "rix",
+    "purrr"
+  ),
+  system_pkgs = "hugo",
+  ide = "none",
+  overwrite = TRUE
+)
